65840d9c9f2ede0ac82ce0bae7444aa487ed27fd
ahencky <Afton1@Aftons-MacBook-Pro.local>
2014-07-09 23:38:12 -0700

    added icons

--- aaa/scripts/home.js
+++ aaa/scripts/home.js
@@ -3,8 +3,6 @@ $(document).ready(function(){
    var containerHeight = $('div.container').height();
    var bottomHeight  = $('#bottom-nav').height();
    var navHeight = containerHeight - bottomHeight;
-   console.log("navHeight: ", navHeight);
-   console.log($(window).scrollTop());
          if ($(window).scrollTop() > navHeight) {
              $('#bottom-nav').addClass('fixed');
          }
